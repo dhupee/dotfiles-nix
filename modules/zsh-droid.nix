7211567e0b7bd490afbb89dd72b6ce0e90a13c2b
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-15 14:07:27 +0700

    seperate zsh for android and for the desktop

---
+++ modules/zsh-droid.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+
+    home.packages = with pkgs; [
+      hack-font
+    ];
+
+    programs.zsh = {
+        enable = true;
+        initExtra = ''
+            # source ~/.p10k.zsh
+
+            eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/base.json)"
+
+            # Sourcing aliases
+            if [ -d ~/.aliases/ ]; then
+                # Loop through all .sh files in the aliases directory and source them
+                for file in ~/.aliases/*.sh; do
+                    if [ -f "$file" ]; then
+                        source "$file"
+                    fi
+                done
+            else
+                echo "Aliases directory not found"
+            fi
+
+
+            # source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+            '';
+        # initExtra = "source ~/.p10k.zsh";
+        oh-my-zsh = {
+            enable = true;
+            # theme = "robbyrussell";
+            plugins = [
+                "thefuck"
+                "copypath"
+                "docker"
+                "podman"
+                "qrcode"
+                "nvm"
+                "node"
+                "npm"
+                "pyenv"
+                "python"
+                "golang"
+                "zoxide"
+            ];
+        };
+    };
+}
