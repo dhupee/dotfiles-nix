81dfdcb258af629c72d6ece843963abafef3a3e7
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-17 23:56:06 +0700

    pass the download to .local/bin instead

--- init-nix-portable.sh
+++ init-nix-portable.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-curl -L https://github.com/DavHau/nix-portable/releases/latest/download/nix-portable-$(uname -m) >./nix-portable
+TARGET=$HOME/.local/bin/nix-portable
+
+if [ ! -d $HOME/.local/bin ]; then
+    mkdir $HOME/.local/bin
+fi
+
+curl -L https://github.com/DavHau/nix-portable/releases/latest/download/nix-portable-$(uname -m) >$TARGET
 
 # if .nix-portable is not executable, make it executable
 if [ ! -x ./nix-portable ]; then
-    chmod +x ./nix-portable
+    chmod +x $TARGET
 fi
