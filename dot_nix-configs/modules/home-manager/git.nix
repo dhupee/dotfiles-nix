81aa290c8e7ad17e792bd76a21f06afc5aefb901
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-18 17:57:44 +0700

    add git modules

---
+++ dot_nix-configs/modules/home-manager/git.nix
@@ -0,0 +1,20 @@
+{ pkgs, ...}:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Daffa Haj Tsaqif";
+    userEmail = "narutohaj00@gmail.com";
+    lfs = {
+      enable = true;
+    extraConfig = {
+        ''
+        [credential "https://github.com"]
+                helper = !$(which gh) auth git-credential
+        [credential "https://gist.github.com"]
+                helper = !$(which gh) auth git-credential
+        ''
+      };
+    };
+  };
+}
