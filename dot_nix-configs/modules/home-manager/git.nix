d70935fcf25252ba2d05e5a2cafb0e121d6982ba
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-19 02:16:16 +0700

    braces issue

--- dot_nix-configs/modules/home-manager/git.nix
+++ dot_nix-configs/modules/home-manager/git.nix
@@ -5,10 +5,10 @@
     enable = true;
     lfs = {
       enable = true;
+    };
     extraConfig = {
         user.name = "Daffa Haj Tsaqif";
         user.email = "narutohaj00@gmail.com";
-      };
     };
   };
 }
