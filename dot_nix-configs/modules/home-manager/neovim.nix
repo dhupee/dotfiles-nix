52a6f57e58f4010813279f0db7305fe5499578a2
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-18 12:35:25 +0700

    add neovim to the nix

---
+++ dot_nix-configs/modules/home-manager/neovim.nix
@@ -0,0 +1,38 @@
+{
+  programs.neovim = {
+    enable = true;
+    package = pkgs.unstable.neovim-unwrapped;
+    defaultEditor = true;
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+
+    extraPackages = with pkgs.unstable; [
+      alejandra
+      black
+      golangci-lint
+      gopls
+      gotools
+      hadolint
+      isort
+      lua-language-server
+      markdownlint-cli
+      nixd
+      nodePackages.bash-language-server
+      nodePackages.prettier
+      pyright
+      ruff
+      shellcheck
+      shfmt
+      stylua
+      yaml-language-server
+    ];
+  };
+
+  xdg.configFile = {
+    "nvim" = {
+      source = "../../config/nvim";
+      recursive = true;
+    };
+  };
+}
